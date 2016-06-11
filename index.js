1499ebe60e0135ec15eb5e7fff16f37a0c38a9c8
Jacob Rask <jacob@jacobrask.net>
2016-06-12 00:08:27 +0200

    Use project structure recommended by eslint yeoman generator

--- index.js
+++ index.js
@@ -2,49 +2,13 @@
 
 module.exports = {
     rules: {
-        "sort-object-props": function(context) {
-            var caseSensitive = context.options[0].caseSensitive;
-            var ignoreMethods = context.options[0].ignoreMethods;
-            var ignorePrivate = context.options[0].ignorePrivate;
-
-            var MSG = "Property names in object literals should be sorted";
-            return {
-                "ObjectExpression": function(node) {
-                    node.properties.reduce(function(lastProp, prop) {
-                        if (ignoreMethods &&
-                                prop.value.type === "FunctionExpression") {
-                            return prop;
-                        }
-                        if (prop.type === 'ExperimentalSpreadProperty') {
-                            return prop;
-                        }
-                        var lastPropId, propId;
-                        if (prop.key.type === "Identifier") {
-                            lastPropId = lastProp.key.name;
-                            propId = prop.key.name;
-                        } else if (prop.key.type === "Literal") {
-                            lastPropId = lastProp.key.value;
-                            propId = prop.key.value;
-                        }
-                        if ((caseSensitive) && (lastPropId !== undefined)) {
-                            lastPropId = lastPropId.toLowerCase();
-                            propId = propId.toLowerCase();
-                        }
-
-                        if (ignorePrivate && /^_/.test(propId)) {
-                          return prop;
-                        }
-
-                        if (propId < lastPropId) {
-                            context.report(prop, MSG);
-                        }
-                        return prop;
-                    }, node.properties[0]);
-                }
-            };
-        }
+        "sort-object-props": require("./lib/rules/sort-object-props")
     },
-    rulesConfig: {
-        "sort-object-props": [ 1, { caseSensitive: false, ignoreMethods: false } ]
+    configs: {
+        recommended: {
+            rules: {
+                "sort-object-props": [1, { caseSensitive: false, ignoreMethods: false }]
+            }
+        }
     }
 };
